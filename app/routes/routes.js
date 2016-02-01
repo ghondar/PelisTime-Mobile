1b87e7b44eae227f959cdee12f8c05f8e10f52b6
Anthony Brian Gavilan Vinces <abgv9221@gmail.com>
2016-02-01 00:26:21 -0500

    [ UPDATE ] Navigator new structure

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,21 +1,27 @@
 'use strict'
 
 import React, {
+  PropTypes,
   StyleSheet,
   Component,
   View,
-  Navigator,
-  BackAndroid
+  Text,
+  ScrollView,
+  DrawerLayoutAndroid,
+  Navigator
 } from 'react-native'
 import dimensions from 'Dimensions'
 import { Toolbar } from 'react-native-material-design'
 
+import Navigation from '../components/Navigation'
+import Navigate from '../utils/Navigate'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import Dashboard from '../containers/Dashboard'
-import DetailVideo from '../containers/DetailVideo'
-import ListVideoContainer from '../containers/ListVideoContainer'
+import * as viewActions from '../actions/viewActions'
+import * as videoActions from '../actions/videoActions'
+
+import ToolbarAndroid from '../components/ToolbarAndroid'
 
 const SCREEN_WIDTH = dimensions.get('window').width
 const BaseConfig = Navigator.SceneConfigs.FloatFromRight
@@ -31,59 +37,89 @@ const CustomSceneConfig = Object.assign({}, BaseConfig, {
   }
 })
 
+const styles = {
+  scene: {
+    flex     : 1,
+    marginTop: 56
+  }
+}
+
 class Route extends Component {
 
   constructor(props, context) {
     super(props, context)
-    this.id = 1
-  }
-
-  componentDidMount() {
-    BackAndroid.addEventListener('hardwareBackPress', this._hardwareBackPress.bind(this))
-  }
-
-  _hardwareBackPress() {
-    if (this.id == 1) {
-      BackAndroid.exitApp()
-      return false
-    } else {
-      this.refs.navigator.jumpBack()
-      return true
+    this.state = {
+      drawer   : null,
+      navigator: null
     }
   }
 
-  _configureScene(route) {
-    return CustomSceneConfig
+  getChildContext() {
+    return {
+      drawer   : this.state.drawer,
+      navigator: this.state.navigator
+    }
   }
 
-  _renderScene(route, navigator) {
-    const { videoStore, detailStore, viewStore } = this.props
-    const data = route.json || {}
-    this.id = route.id
+  setDrawer(drawer) {
+    this.setState({
+      drawer
+    })
+  }
 
-    switch (route.id){
-      case 1:
-        return <ListVideoContainer navigator={navigator} location={data}/>
-
-      case 2:
-        return <DetailVideo navigator={navigator} location={data}/>
-    }
+  setNavigator(navigator) {
+    this.setState({
+      navigator: new Navigate(navigator)
+    })
   }
 
   render() {
+    const { viewStore, dispatch, videoStore } = this.props
+    const { drawer, navigator } = this.state
+
     return (
-      <Navigator
-        ref='navigator'
-        initialRoute={{
-                        name: 'dashboard',
-                        id  : 1
-                      }}
-        navigationBar={<Toolbar title='PelisTime' icon='menu'/>}
-        configureScene={this._configureScene.bind(this)}
-        renderScene={this._renderScene.bind(this)}
-      />
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => {
+          if (drawer && navigator) {
+            return <Navigation
+                      videoStore= {videoStore}
+                      viewStore= {viewStore}
+                      {...bindActionCreators(viewActions, dispatch)}
+                      {...bindActionCreators(videoActions, dispatch)}/>
+          }
+
+          return null
+        }}
+        ref={(drawer) => { !this.state.drawer ? this.setDrawer(drawer) : null }}>
+        {drawer &&
+        <Navigator
+          initialRoute={Navigate.getInitialRoute()}
+          navigationBar={<ToolbarAndroid viewStore= {viewStore} onIconPress={drawer.openDrawer} />}
+          configureScene={() => Navigator.SceneConfigs.FadeAndroid }
+          ref={(navigator) => { !this.state.navigator ? this.setNavigator(navigator) : null }}
+          renderScene={(route) => {
+            if (this.state.navigator && route.component) {
+              return (
+                <View
+                  style={styles.scene}
+                  showsVerticalScrollIndicator={false}>
+                  <route.component title={route.title} path={route.path} {...route.props} />
+                </View>
+              )
+            }
+          }}
+        />
+        }
+      </DrawerLayoutAndroid>
     )
   }
+}
+
+Route.childContextTypes = {
+  drawer   : PropTypes.object,
+  navigator: PropTypes.object
 }
 
 export default connect(state => state)(Route)
