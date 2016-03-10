15553f221b9574e77fb2a2f00a219a4adae29a16
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2016-03-10 15:32:47 -0500

    [ CHANGE ] api

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -1,7 +1,7 @@
 import Q from 'q'
 import request from 'superagent'
 
-const API_ROOT = 'http://api-ghondar.mod.bz'
+const API_ROOT = 'http://api-ghondar.jit.su'
 
 export function fetchList(json, page) {
   var deferred = Q.defer()
