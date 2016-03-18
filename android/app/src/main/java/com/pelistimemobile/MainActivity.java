0ea60315ab61d3374c30458010151231dcda49ee
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2016-03-18 01:48:51 -0500

    [ ADD ] auto updater

--- android/app/src/main/java/com/pelistimemobile/MainActivity.java
+++ android/app/src/main/java/com/pelistimemobile/MainActivity.java
@@ -10,7 +10,82 @@ import java.util.List;
 import com.ghondar.torrentstreamer.TorrentStreamerPackage;
 import me.neo.react.StatusBarPackage;
 
-public class MainActivity extends ReactActivity {
+import com.aerofs.reactnativeautoupdater.ReactNativeAutoUpdater;
+import com.aerofs.reactnativeautoupdater.ReactNativeAutoUpdater.ReactNativeAutoUpdaterUpdateType;
+import com.aerofs.reactnativeautoupdater.ReactNativeAutoUpdater.ReactNativeAutoUpdaterFrequency;
+import com.aerofs.reactnativeautoupdater.ReactNativeAutoUpdaterActivity;
+import com.aerofs.reactnativeautoupdater.ReactNativeAutoUpdaterPackage;
+
+public class MainActivity extends ReactNativeAutoUpdaterActivity {
+    /**
+     *  Name of the JS Bundle file shipped with the app.
+     *  This file has to be added as an Android Asset.
+     * */
+    @Nullable
+    @Override
+    protected String getBundleAssetName() {
+        return "main.android.jsbundle";
+    }
+
+    /**
+     *  URL for the metadata of the update.
+     * */
+    @Override
+    protected String getUpdateMetadataUrl() {
+        return "https://dl.dropboxusercontent.com/u/35686843/Pelis%20Time/update.android.json";
+    }
+
+    /**
+     * Name of the metadata file shipped with the app.
+     * This metadata is used to compare the shipped JS code against the updates.
+     * */
+    @Override
+    protected String getMetadataAssetName() {
+        return "metadata.android.json";
+    }
+
+    /**
+     *  If your updates metadata JSON has a relative URL for downloading
+     *  the JS bundle, set this hostname.
+     * */
+    @Override
+    protected String getHostnameForRelativeDownloadURLs() {
+        return "https://www.dropbox.com";
+    }
+
+    /**
+     *  Decide what type of updates to download.
+     * Available options -
+     *  MAJOR - will download only if major version number changes
+     *  MINOR - will download if major or minor version number changes
+     *  PATCH - will download for any version change
+     * default value - PATCH
+     * */
+    @Override
+    protected ReactNativeAutoUpdaterUpdateType getAllowedUpdateType() {
+        return ReactNativeAutoUpdater.ReactNativeAutoUpdaterUpdateType.MINOR;
+    }
+
+    /**
+     *  Decide how frequently to check for updates.
+     * Available options -
+     *  EACH_TIME - each time the app starts
+     *  DAILY     - maximum once per day
+     *  WEEKLY    - maximum once per week
+     * default value - EACH_TIME
+     * */
+    @Override
+    protected ReactNativeAutoUpdaterFrequency getUpdateFrequency() {
+        return ReactNativeAutoUpdaterFrequency.EACH_TIME;
+    }
+
+    /**
+     *  To show progress during the update process.
+     * */
+    @Override
+    protected boolean getShowProgress() {
+        return true;
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -37,6 +112,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
+            new ReactNativeAutoUpdaterPackage(),
             new MainReactPackage(),
             new TorrentStreamerPackage(),
             new StatusBarPackage(this)
